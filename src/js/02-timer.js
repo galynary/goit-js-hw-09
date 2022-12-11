a5acba90b497b3ec4ccfd5de9aee140a156afa38
galynary <107271972+galynary@users.noreply.github.com>
2022-12-11 18:46:19 +0200

    change js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -34,9 +34,7 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
 }
 
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
 
 function pad(value) {
     return String(value).padStart(2, '0');
@@ -85,14 +83,12 @@ class Timer  {
                 daysEl.textContent = components.days;
                 if (deltaTime < 1000) {
                     this.stopTimer();
-                }
-                    this.updateComponentsTimer(components);
-              
-                 
+                   
+                } 
     }, 1000)
 
     }
-    timerStop() {
+    stopTimer() {
         clearInterval(this.timerId);
     }
 }
